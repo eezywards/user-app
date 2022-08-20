08efcf90aebc1a31640f020674ba749450c8b618
Alan Aguirre <a01657644@itesm.mx>
2022-08-20 06:38:20 -0500

    Footer
    Missing media q

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <div>
     <Navbar />
-    
+    <section>
+    <Footer />
+    </section>
+    </div>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+// Footer.js
+import "../css/footer.css";
+import "../css/main.css";
+
+export default function Footer() {
+    return (
+        <div class="footer-container">
+        <div class="row">
+        <div class="col-sm-5">
+            <div class="row align-items-center">
+                <div class="footer-title col-sm-12" >Eezywards</div></div>
+             <div class="footer-text row align-items-center"><div class="col-sm-12">Decentralized coupons and rewards center bridging Web3 and Web2</div></div>
+        </div>
+
+        <div class="col-sm-2">
+        <div class="row align-items-center">
+                <div class="footer-subtitle col-sm-12">Quick Links</div></div>
+             <div class="footer-text row align-items-center"><div class="col-sm-12"> About <br/> Advantages  <br/> Use now
+             </div></div>
+
+        </div>
+
+        <div class="col-sm-2">
+        <div class="row align-items-center">
+                <div class="footer-subtitle col-sm-12">Tools </div></div>
+             <div class="footer-text row align-items-center"><div class="col-sm-12"> Polygon <br/> WalletConnect  <br/> Worldcoin  <br/> IPFS
+             </div></div>
+        </div>
+
+        <div class="col-sm-3">
+        <div class="row align-items-center">
+                <div class="footer-subtitle col-sm-12">Contact us </div></div>
+             <div class="footer-text row align-items-center"><div class="col-sm-12"> contact@eezywards.com 
+             </div></div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="footer-text low-row-right col-sm-6">Eezywards 2022</div>
+        <div class="footer-text low-row-left col-sm-6">ETHGlobal 2022</div>
+        </div>
+      </div>
+    );
+  }
+  
