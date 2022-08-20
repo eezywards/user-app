f255b326e72d6e1d7502e76aef8e557304e9f019
Esteban Zepeda <estzepeda@outlook.com>
2022-08-20 05:20:00 -0500

    Landing page update

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './css/main.css';
+import './css/home.css';
+
+
+
+function App() {
+  return (
+    <main>
+      <header>
+      <div className="blue-stripe"></div>
+        <div className="container">
+          <div className="top-items">
+            <p className="texts-t1">
+              The future of rewards system
+            </p>
+            <p className="texts-t2">
+              Decentralized coupons and rewards center bridging Web3 and Web2.
+            </p>
+            <button className="btn-type-1 texts-l">Sign up</button>
+          </div>
+          <div className="bottom-items">
+            <ul className="network-logos">
+              {/*Worldcoin*/}
+              <li>
+                <a href="" target="_blank">
+                  <img src="./img/logo/worldcoin-logo-blue.png" />
+                </a>
+              </li>
+              {/*Polygon*/}
+              <li>
+                <a href="" target="_blank">
+                  <img src="./img/logo/polygon-logo-blue.png" />
+                </a>
+              </li>
+              {/*IPFS*/}
+              <li>
+                <a href="" target="_blank">
+                  <img src="./img/logo/ipfs-logo-blue.png" />
+                </a>
+              </li>
+              {/*Wallet Connect*/}
+              <li>
+                <a href="" target="_blank">
+                  <img src="./img/logo/walletconnect-logo-blue.png" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </main>
+  );
+}
+
+export default App;
